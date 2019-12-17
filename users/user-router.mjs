06f938794a290ce5078e3ba030221e29ab75ea0e
drejohnson <drejohnson212@gmail.com>
2019-12-17 18:50:23 -0500

    use restricted middleware on user routes

--- users/user-router.mjs
+++ users/user-router.mjs
@@ -1,9 +1,10 @@
 import express from 'express';
 import UsersModel from './user-model.mjs';
+import restricted from '../auth/restricted-middleware.mjs';
 
 const router = express.Router()
 
-router.get('/', async (req, res) => {
+router.get('/', restricted, async (req, res) => {
   try {
     const users = await UsersModel.findUser()
     res.status(200).json(users)
@@ -12,13 +13,13 @@ router.get('/', async (req, res) => {
   }
 })
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', restricted, async (req, res) => {
   const { id } = req.params;
   try {
     const user = await UsersModel.findById(id);
-    if (!user)
-      return res.status(404).json({ message: 'user not found' })
-    res.status(200).json(user);
+    !user
+      ? res.status(404).json({ message: 'user not found' })
+      : res.status(200).json(user);
   } catch (error) {
     res.status(400).send(error);
   }
